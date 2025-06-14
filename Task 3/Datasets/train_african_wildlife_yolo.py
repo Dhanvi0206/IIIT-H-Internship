691798d841bc25656bc3771c574cdafda9bc0e7b
Dhanvi0206 <149807947+Dhanvi0206@users.noreply.github.com>
2025-06-14 22:52:41 +0530

    Delete Task 3/Datasets/train_african_wildlife_yolo.py

--- Task 3/Datasets/train_african_wildlife_yolo.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-
-# Train the model
-results = model.train(data="african-wildlife.yaml", epochs=15, imgsz=640)
